a8b604f09367f4ad2df3239bab1a5ac33386fb18
JollyRen <jollyren@gmail.com>
2024-01-03 19:24:12 -0700

    made error codes file and replaced those, threw in a switch instead of if else blocks for getting user details, misc on auth.js, user.js (db), and user.js (routes), jwt, and index. Fixed try...catch error throwing bug

--- jwt.js
+++ jwt.js
@@ -1,30 +1,20 @@
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcrypt'
 import { createCipheriv, createDecipheriv } from 'crypto'
+import { tokenMalformed, authHeaderError } from './errorCodes.js'
 const { verify, sign } = jwt
 
-const { JWT_SECRET, EMAIL_KEY: CIPHER_KEY } = process.env
+const { JWT_SECRET, CIPHER_KEY } = process.env
 
 export const getIdFromToken = (auth) => {
   const prefix = 'Bearer '
-  if (!auth.startsWith(prefix))
-    throw new Error({
-      name: 'TokenMalformed',
-      message: `Authorization token must start with '${prefix}'`,
-      status: 400
-    })
+  if (!auth.startsWith(prefix)) throw tokenMalformed(prefix)
 
   const id = verify(auth.slice(prefix.length), JWT_SECRET)
-  if (!id)
-    throw new Error({
-      name: 'AuthorizationHeaderError',
-      message: 'Authorization token malformed',
-      status: 401
-    })
+  if (!id) throw authHeaderError
 
   return id
 }
-
 export const getTokenFromId = (id) => sign(id, JWT_SECRET, { expiresIn: '1w' })
 
 export const getCipherFromText = (text) => {
@@ -32,17 +22,13 @@ export const getCipherFromText = (text) => {
   const emailCipher = cipher.update(text, 'utf8', 'hex') + cipher.final('hex')
   return emailCipher
 }
-
 export const getTextFromCipher = (encryption) => {
   const decipher = createDecipheriv('aes-256-cbc', CIPHER_KEY, null)
   const email = decipher.update(encryption, 'hex', 'utf8') + decipher.final('utf8')
   return email
 }
 
-// export const getEmailFromHash = (hash) =>
-
 export const hashPass = async (password) => await bcrypt.hash(password, 10)
-
 export const verifyPass = async (password, hashedPass) => bcrypt.compare(password, hashedPass)
 
 export default jwt
