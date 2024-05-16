38014a06db28e64b86f7620aa1390036fc2d1d70
JollyRen <jollren@gmail.com>
2024-05-16 14:16:14 -0600

    auth and user router adjustments - user can get themselves from token now

--- routes/user.js
+++ routes/user.js
@@ -18,10 +18,11 @@ const userRouter = express.Router()
  *
  */
 
-userRouter.get('/', async (req, res, next) => {
+userRouter.get('/admin', async (req, res, next) => {
   try {
     if (!req.local.user) throw noUser
     if (!req.local.user.admin) throw notAdmin
+    res.status(200).send(req.local.user)
     // get all users if user is admin
     // skip and take queries for pagination - 10 per page default?
     // user (id, name, email, phone, admin, active, subs)
@@ -29,16 +30,39 @@ userRouter.get('/', async (req, res, next) => {
     next(err)
   }
 })
-userRouter.get('/:userId', async (req, res, next) => {
-  // get specific user (self or admin)
+userRouter.get('/me', async (req, res, next) => {
+  try {
+    if (!req.local.user) throw noUser
+    res.status(200).send(req.local.user)
+    // get your own user data if you're the user
+    // user (id, name, email, phone, admin, active, subs)
+  } catch (err) {
+    next(err)
+  }
+})
+userRouter.get('/admin/:userId', async (req, res, next) => {
+  // get specific user (admin)
   // get request params
   // validate req.local.user is same user
   // only for updating user auth details
   // updateUser(id, {username, password})
   // retrieve revalidated user by id
-  // send user (name, email, phone, admin, active, subs)
+  // send user (id, name, email, phone, admin, active, subs)
 })
-userRouter.put('/:userId', async (req, res, next) => {
+userRouter.put('/admin/:userId', async (req, res, next) => {
+  // update user details (self or admin)
+  // get request params
+  //   admin or user does auth check
+  //   returned user from username / password must be admin or self
+  //   used for buying subs, deletion, or updates
+  // get request body (name, email, phone)
+  // validate req.local.user is same user or admin
+  // only for updating user auth details
+  // updateUser(id, {name, email, phone})
+  // retrieve revalidated user by id
+  // send user same as GET
+})
+userRouter.put('/update/:userId', async (req, res, next) => {
   // update user details (self or admin)
   // get request params
   //   admin or user does auth check
@@ -52,9 +76,9 @@ userRouter.put('/:userId', async (req, res, next) => {
   // send user same as GET
 })
 
-userRouter.delete('/:userId', async (req, res, next) => {
+userRouter.delete('/delete/:userId', async (req, res, next) => {
   // delete user - make inactive
-  // use Auth route instead?
+  // user-requested
 })
 
 export default userRouter
