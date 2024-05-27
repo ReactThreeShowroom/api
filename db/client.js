8af40a3201a4e56ce6af5b5382039c58c35cb79c
JollyRen <jollren@gmail.com>
2024-05-27 13:53:57 -0600

    added client, item, user db functions to fill in.edited schema to add sub info (status)

--- db/client.js
+++ db/client.js
@@ -1,3 +1,6 @@
+import prisma from '../prismaClient.js'
+import { getCipherFromText, getTextFromCipher } from '../jwt.js'
+
 // model Client {
 //   id      String         @id @unique @default(uuid())
 //   name    String
@@ -7,3 +10,26 @@
 //   userId  String
 //   User    User           @relation(fields: [userId], references: [id])
 // }
+
+// hash name, email, phone
+
+// search by email / name MAY be possible by hashing email / name first, then searching for hash
+// need to experiment with the idea also for applicator searches
+
+export const createClient = async () => {}
+
+export const updateClient = async () => {}
+
+export const deleteClient = async () => {}
+
+export const createChoice = async () => {}
+
+export const getChoice = async () => {}
+
+export const updateChoice = async () => {}
+
+export const deleteChoice = async () => {}
+
+export const getClientsByUser = async () => {}
+
+export const getclientById = async () => {}
---
+++ db/item.js
@@ -0,0 +1,29 @@
+import prisma from '../prismaClient.js'
+
+export const createColor = async () => {}
+
+export const getColorByName = async () => {}
+
+export const getColorByCode = async () => {}
+
+export const editColor = async () => {}
+
+export const deleteColor = async () => {}
+
+export const addItem = async () => {}
+
+export const getItemByName = async () => {}
+
+export const getItemByType = async () => {}
+
+export const getItemBySubtype = async () => {}
+
+export const editItem = async () => {}
+
+export const deleteItem = async () => {}
+
+export const addPattern = async () => {}
+
+export const editPattern = async () => {}
+
+export const deletePattern = async () => {}
